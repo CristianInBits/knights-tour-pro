f738474f86ff64da1a6dec5f7d2de193cf373fe8
Cristian Laurentiu SINDILA <128726794+CristianInBits@users.noreply.github.com>
2025-07-27 22:48:31 +0200

    Add KnightMove utility class (#18)

---
+++ src/main/java/knights/model/KnightMove.java
@@ -0,0 +1,24 @@
+package knights.model;
+
+import java.util.List;
+
+/**
+ * Static utility for knight's possible move offsets.
+ */
+public final class KnightMove {
+
+    public static final int[] DX = { -2, -1, 1, 2,  2,  1, -1, -2 };
+    public static final int[] DY = {  1,  2, 2, 1, -1, -2, -2, -1 };
+
+    public static final int TOTAL_MOVES = 8;
+
+    private KnightMove() {
+        // Prevent instantiation
+    }
+
+    public static List<Position> generateNextPositions(Position from) {
+        return java.util.stream.IntStream.range(0, TOTAL_MOVES)
+            .mapToObj(i -> new Position(from.row() + DX[i], from.col() + DY[i]))
+            .toList();
+    }
+}
