20fa62b3e2e3f24817e6f9af52a65ba8cf0f0c11
CristianInBits <128726794+CristianInBits@users.noreply.github.com>
2025-08-01 22:34:43 +0200

    feat: backtrack & warnsdorff benchmark & updated dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     java
     application
+    id("me.champeau.jmh") version "0.7.2"
 }
 
 group = "knights"
@@ -20,8 +21,10 @@ repositories {
 }
 
 dependencies {
-    implementation ("com.google.code.gson:gson:2.10.1")
+    implementation("com.google.code.gson:gson:2.10.1")
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
+    jmh("org.openjdk.jmh:jmh-core:1.37")
+    jmh("org.openjdk.jmh:jmh-generator-annprocess:1.37")
 }
 
 application {
@@ -34,4 +37,17 @@ tasks.jar {
     }
     duplicatesStrategy = DuplicatesStrategy.EXCLUDE
     from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
+}
+
+tasks.withType<JavaCompile> {
+    options.compilerArgs.addAll(listOf("-Xlint:unchecked", "-Xlint:deprecation"))
+}
+
+// Enable benchmarking
+jmh {
+    warmupIterations.set(3)
+    iterations.set(5)
+    fork.set(1)
+    benchmarkMode.set(listOf("Throughput"))
+    timeUnit.set("ms")
 }
